7346be526440e684257c98aa59a3c1b471c8a7c0
rubensantibanezacosta <rubensantibanezacosta902@gmail.com>
2022-03-01 15:06:04 +0000

    qr generator

--- odoo_whatsapp_integration/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import sale_fun
-from . import inventory_fun
-from . import purchase_fun
-from . import invoice_fun
-from . import contacts_fun
-from . import setting_inherit
--- odoo_whatsapp_integration/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import message_wizard
-from . import message_wizard_contact
-from . import share_action
-from . import message_wizard_multiple_contact
